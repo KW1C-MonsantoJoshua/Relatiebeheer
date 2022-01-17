cbd198852b2cc70b075644b5ce6516dca3882699
TeusROC <40191531@roctilburg.nl>
2022-01-17 16:34:04 +0100

    Postcode checker toevoegen

--- PostcodeChecker.php
+++ PostcodeChecker.php
@@ -1,8 +1,8 @@
 <?php
-$pc		= "1234AB";		// vul een geldige postcode in
-$hn		= 1;			// vul een geldig huisnummer in
-$tv		= "a";			// vul een geldige toevoeging in of laat leeg
-$ak		= "apikey";		// vul hier uw eigen key in (zie registratie hieronder).
+$pc		= "5043WH";		// vul een geldige postcode in
+$hn		= 109;			// vul een geldig huisnummer in
+$tv		= "";			// vul een geldige toevoeging in of laat leeg
+$ak		= "FbW29C_969cyVfAKrj";		// vul hier uw eigen key in (zie registratie hieronder).
 
 $getadrlnk	= "https://bwnr.nl/postcode.php?pc=".urlencode($pc)."&hn=".urlencode($hn)."&tv=".urlencode($tv)."&tg=data&ac=pc2adres&ak=$ak";
 
@@ -16,7 +16,7 @@ if ($result=="Geen resultaat.") {echo $result;} else {
     $lon	= $adres[3];
     $gm	= $adres[4];
     echo "
-		straat1		: $str<br>
+		straat		: $str<br>
 		plaats		: $pl<br>
 		lat 		: $lat<br>
 		lon 		: $lon<br>
