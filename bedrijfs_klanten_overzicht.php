0f264ac40d7938840d17bcebe776343bcc2ec70d
TeusROC <40191531@roctilburg.nl>
2022-01-18 13:13:24 +0100

    Postcode checker klaar

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -1086,10 +1086,7 @@ include "partials/footer.php";
 ?>
 <script>
     /*
-        Notice aan 'slimme programmeurs' die denken de hier gebruikte apikey te kunnen achterhalen en te gebruiken:
-        Spaar je de moeite. Je kunt inderdaad die apikey achterhalen. Maar het gaat toch niet werken. Het werkt alleen met een zelf aangevraagde apikey.
-        Dus vraag die apikey gewoon aan, gebruik de gratis calls om te testen. Kleine moeite, groot gemak.
-        Bijzondere situatie? Neem gewoon contact op.
+        zet script nog in apart bestand wnnr af voor de aapie
     */
     var e		= "FbW29C_969cyVfAKrj";
     var postcode	= "";
@@ -1099,14 +1096,14 @@ include "partials/footer.php";
     function check_pc(wat,waarde) {
         if (wat=="postcode") {
             var pc = waarde.trim();
-            if (pc.length <6) {maak_leeg();return;}					// POSTCODE MOET MINIMAAL 6 CHARACTERS BEVATTEN
+            if (pc.length <6) {maak_leeg();return;}					// moet minimaal 6 characters hebben
             var num_deel = pc.substr(0,4);
-            if (parseFloat(num_deel) < 1000) {maak_leeg();return;}	// HET NUMERIEKE DEEL MOET MINIMAAL 1000 ZIJN
+            if (parseFloat(num_deel) < 1000) {maak_leeg();return;}	// moet minaal 1000 zijn
             var alpha_deel = pc.substr(-2);
             if (alpha_deel.charCodeAt(0) < 65 || alpha_deel.charCodeAt(0) > 122 || alpha_deel.charCodeAt(1) < 65 || alpha_deel.charCodeAt(1) > 122 ) {maak_leeg();return;} 	// DE LAATSTE 2 POSITIES MOETEN LETTERS ZIJN
             alpha_deel = alpha_deel.toUpperCase();
 
-            // NU WETEN WE ZEKER EEN POSTCODE TE HEBBEN DIE BEGINT MET 4 LETTERS EN EINDIGT OP 2 CIJFERS
+            // de checker niffo
 
             postcode = num_deel+alpha_deel;
             document.getElementById("postcode_p").value = postcode;
@@ -1143,7 +1140,7 @@ include "partials/footer.php";
                 if (rString=="Onbekende API Key.") {maak_leeg();return;}
                 if (rString=="Over quota") {maak_leeg();return;}
                 if (rString=="Onjuiste API Key.") {maak_leeg();alert('Alleen functioneel indien geopend vanuit de API pagina. Ga terug naar de API pagina en probeer opnieuw.');return;}
-
+                // 0 = straat - 1 = plaats
                 aResponse = rString.split(";");
                 document.getElementById("straat_p").value=aResponse[0];
                 document.getElementById("straat_z").value=aResponse[0];
@@ -1166,7 +1163,6 @@ include "partials/footer.php";
         document.getElementById("straat_w").value="";
         document.getElementById("plaats_w").value="";
     }
-
 </script>
 
 </body>
