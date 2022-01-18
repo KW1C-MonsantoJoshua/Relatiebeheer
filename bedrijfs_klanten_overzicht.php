7ae0fe53d7ac324232363fb15234115c7c671e92
TeusROC <40191531@roctilburg.nl>
2022-01-18 11:02:51 +0100

    Postcode checker toevoegen

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -1218,7 +1218,7 @@ include "partials/footer.php";
 
     function maak_leeg() {
         document.getElementById("straat_z").value="";
-        document.getElementById("plaats_p").value="";
+        document.getElementById("plaats_z").value="";
     }
 
 </script>
