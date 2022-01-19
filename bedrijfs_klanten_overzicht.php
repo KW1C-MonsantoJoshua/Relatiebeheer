ebe0f8b5ae5cf9d06efbfb58ab2be731c3d9c8dc
TeusROC <40191531@roctilburg.nl>
2022-01-19 09:41:23 +0100

    wijziging form huisnummer naast toevoeging

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION["loggedin"])) {
     br {
         line-height: 75%;
     }
-    ::placeholder {
+    input::placeholder {
         color: red;
     }
 </style>
@@ -760,7 +760,7 @@ include "partials/navbar.php";
                                                                                aria-invalid="false" name="straatnaam_p">
                                                                     </div>
 
-                                                                </div>
+
                                                                 </div>
                                                             </div>
                                                             <div class="col-12 col-md-4">
