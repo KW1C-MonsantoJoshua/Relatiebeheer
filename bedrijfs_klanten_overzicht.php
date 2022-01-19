b8f0ea9afa512052b6cf75bd4099ca05e4c89fc3
TeusROC <40191531@roctilburg.nl>
2022-01-19 13:18:29 +0100

    wijziging form huisnummer naast toevoeging

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -788,7 +788,6 @@ include "partials/navbar.php";
                                                                 </div>
                                                             </div>
                                                             <div class="col-12 d-flex flex-sm-row flex-column justify-content-end mt-3 mt-sm-2">
-                                                                <div class="modal-footer">
                                                                 <input type="submit"
                                                                        class="btn btn-primary mb-2 mb-sm-0 mr-sm-2"
                                                                        name="registreerParticulier"
@@ -798,7 +797,6 @@ include "partials/navbar.php";
                                                                         data-dismiss="modal"
                                                                         class="btn btn-secondary">Cancel
                                                                 </button>
-
                                                             </div>
                                                         </div>
                                                     </form>
