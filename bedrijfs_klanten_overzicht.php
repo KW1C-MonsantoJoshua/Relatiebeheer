288282e7ebb18d6b66b2a859dcd8aa10f354fd2b
TeusROC <40191531@roctilburg.nl>
2022-01-19 13:14:27 +0100

    wijziging form huisnummer naast toevoeging

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -788,7 +788,7 @@ include "partials/navbar.php";
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                        <div class="col-12 d-flex flex-sm-row flex-column justify-content-end mt-3 mt-sm-2">
+                                                        <div class="d-flex flex-sm-row flex-column justify-content-end mt-3 mt-sm-2">
                                                                 <div class="modal-footer">
                                                                 <input type="submit"
                                                                        class="btn btn-primary mb-2 mb-sm-0 mr-sm-2"
