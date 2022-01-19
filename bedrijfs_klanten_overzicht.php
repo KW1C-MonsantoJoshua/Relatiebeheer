86f4aed456e45ecc1da65fc3259ee75839299359
TeusROC <40191531@roctilburg.nl>
2022-01-19 14:10:01 +0100

    wijziging form huisnummer naast toevoeging

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -928,6 +928,8 @@ include "partials/navbar.php";
                                                                 </div>
                                                             </div>
                                                         </div>
+                                                    </br>
+                                                    </br>
                                                         <div class="modal-footer">
                                                             <button type="reset"
                                                                     data-dismiss="modal"
