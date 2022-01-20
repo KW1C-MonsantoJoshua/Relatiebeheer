5d38fdd5fecf4c9b57fdf38a0facb168e839f086
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-01-20 09:52:21 +0100

    modal

--- bedrijfs_overzicht.php
+++ bedrijfs_overzicht.php
@@ -3,8 +3,7 @@
 require_once "backend/functions.php";
 
 InsertBedrijf();
-ViewC();
-
+ViewUserP();
 // Controleer of iemand ingelogd is
 if (!isset($_SESSION["loggedin"])) {
     header("Location: index.php");
