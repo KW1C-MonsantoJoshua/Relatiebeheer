ee8b4adae92df69fc879055dc53034db4c508ed1
TeusROC <40191531@roctilburg.nl>
2022-01-31 10:32:30 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -51,13 +51,7 @@ include "partials/navbar.php";
                 </div>
                 <!-- Account Settings starts -->
                 <div class="row">
-                    <div style="
-        position: -webkit-sticky;
-        position: sticky;
-        top: 0;
-        background-color: #666;
-        padding: 40px;
-        font-size: 25px;" class="col-md-3 mt-3">
+                    <div class="col-md-3 mt-3">
                         <!-- Nav tabs -->
                         <ul class=" nav flex-column nav-pills" id="myTab" role="tablist">
                             <li class="nav-item">
