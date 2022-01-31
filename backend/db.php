14b2d79a9f25ab8fcea68fae2bb7864ad2d54e1f
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-01-31 11:48:00 +0100

    database connectie update test

--- backend/db.php
+++ backend/db.php
@@ -1,19 +1,30 @@
 <?php
-$servername = "localhost";
-$username = "relatebeheer";
-$password = "Z1HaCog5gh6d%efu";
-$db = "relatebeheer";
+//$servername = "localhost";
+//$username = "relatebeheer";
+//$password = "Z1HaCog5gh6d%efu";
+//$db = "relatebeheer";
+//
+//// Create connection
+//$mysqli = new mysqli("$servername", "$username", "$password", "$db");
+//$con = mysqli_connect($servername, $username, $password, $db);
+//
+//// Check connection
+//if (!$con) {
+//    die("Connection failed: " . mysqli_connect_error());
+//}
+//
+//function getSQLConnect()
+//{
+//    return $mysqli;
+//}
+$host="81.169.139.193";
+$port=9999;
+$socket="";
+$user="qccstest";
+$password="";
+$dbname="relatebeheer";
 
-// Create connection
-$mysqli = new mysqli("$servername", "$username", "$password", "$db");
-$con = mysqli_connect($servername, $username, $password, $db);
+$mysqli = new mysqli($host, $user, $password, $dbname, $port, $socket)
+or die ('Could not connect to the database server' . mysqli_connect_error());
 
-// Check connection
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-function getSQLConnect()
-{
-    return $mysqli;
-}
+//$con->close();
