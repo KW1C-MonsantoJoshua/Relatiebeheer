c5d75b2513d6d06ac1fa3fa7846df0a25c72db66
TeusROC <40191531@roctilburg.nl>
2022-01-31 09:52:19 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -196,6 +196,15 @@ include "partials/navbar.php";
                                                                        placeholder="Straatnaam" required
                                                                        aria-invalid="false" name="straatnaam_z">
                                                             </div>
+                                                            <div class="controls ">
+                                                                <label for="straatnaam">Straatnaam</label>
+                                                                <input type="text" id="straat_z"
+                                                                       class="form-control round"
+                                                                       pattern="[a-zA-Z]{1,15}"
+                                                                       title="Alleen letters"
+                                                                       placeholder="Straatnaam" required
+                                                                       aria-invalid="false" name="straatnaam_z">
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
