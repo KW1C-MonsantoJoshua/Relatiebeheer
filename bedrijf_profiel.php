0b57f960c1b8b1909c15833f2008ffadf082da32
TeusROC <40191531@roctilburg.nl>
2022-01-31 10:09:51 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -218,7 +218,7 @@ include "partials/navbar.php";
                                                     <input type="submit"
                                                            class="btn btn-primary mb-2 mb-sm-0 mr-sm-2"
                                                            name="registreerParticulier"
-                                                           value="Bedrijf gegevens bewerken">
+                                                           value="Opslaan">
                                                 </div>
                                             </form>
                                         </div>
