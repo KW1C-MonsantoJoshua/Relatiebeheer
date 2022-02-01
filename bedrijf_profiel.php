3ac498cbecb18a4f805332f5323cdb48f2a5422a
TeusROC <40191531@roctilburg.nl>
2022-02-01 13:10:17 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -1642,8 +1642,8 @@ include "partials/navbar.php";
                             <div class="col-12 d-flex flex-sm-row flex-column justify-content-end mt-3 mt-sm-2">
                                 <input type="submit"
                                        class="btn btn-primary mb-2 mb-sm-0 mr-sm-2"
-                                       name="registreerBedrijf"
-                                       value="Bedrijf toevoegen">
+                                       name="nieuwenotitie"
+                                       value="nieuwenotitie">
 
                                 <button type="reset"
                                         data-dismiss="modal"
