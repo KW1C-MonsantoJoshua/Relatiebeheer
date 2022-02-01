19e6a06a6af4a5e9ab0912b534d6b6b3866d567d
TeusROC <40191531@roctilburg.nl>
2022-02-01 13:06:33 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -1625,7 +1625,7 @@ include "partials/navbar.php";
                         <div class="row">
                             <div class="col-2 col-md-1">
                                 <div class="form-group">
-                                    <h4>Bedrijfsgegevens</h4>
+                                    <h4>Nieuwe Notitie</h4>
                                     <div class="controls">
                                         <div class="controls ">
                                             <label for="bedrijfsnaam">Onderwerp</label>
