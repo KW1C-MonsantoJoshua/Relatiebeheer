90360bb32a490e6786d6d338a6b91c1fe765da02
TeusROC <40191531@roctilburg.nl>
2022-02-01 13:33:47 +0100

    test paginae

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -1608,99 +1608,103 @@ include "partials/navbar.php";
 
 
 
-<div class="modal fade text-left" id="largechicken" tabindex="-1" role="dialog" aria-labelledby="myModalLabel17"
-     aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
+<div class="modal fade text-left" id="info<?php echo $row['id'] ?>" tabindex="-1" role="dialog"
+     aria-labelledby="myModalLabel17" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="modal-title" id="myModalLabel34">Bedrijf toevoegen</h3>
+                <h4 class="modal-title" id="myModalLabel2"><i class="ft-bookmark mr-2"></i>Klantgegevens</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true"><i class="ft-x font-medium-2 text-bold-700"></i></span>
                 </button>
             </div>
-
-            <div class="col-lg-6 col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Basic Form</h4>
-                    </div>
-                    <div class="card-content">
-                        <div class="card-body">
-                            <p>This is the most basic and default form having inputs, labels and buttons.</p>
-                            <form>
-                                <div class="form-row">
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-1">First Name</label>
-                                            <input type="text" id="basic-form-1" class="form-control" name="fname">
+            <div class="modal-body">
+                <div class="col-lg-6 col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Basic Form</h4>
+                        </div>
+                        <div class="card-content">
+                            <div class="card-body">
+                                <p>This is the most basic and default form having inputs, labels and buttons.</p>
+                                <form>
+                                    <div class="form-row">
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-1">First Name</label>
+                                                <input type="text" id="basic-form-1" class="form-control" name="fname">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-2">Last Name</label>
+                                                <input type="text" id="basic-form-2" class="form-control" name="lname">
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-2">Last Name</label>
-                                            <input type="text" id="basic-form-2" class="form-control" name="lname">
+                                    <div class="form-row">
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-3">E-mail</label>
+                                                <input type="text" id="basic-form-3" class="form-control" name="email">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-4">Contact Number</label>
+                                                <input type="number" id="basic-form-4" class="form-control" name="phone">
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="form-row">
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-3">E-mail</label>
-                                            <input type="text" id="basic-form-3" class="form-control" name="email">
+                                    <div class="form-group mb-2">
+                                        <label for="basic-form-5">Company</label>
+                                        <input type="text" id="basic-form-5" class="form-control" name="company">
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-6">Interested in</label>
+                                                <select id="basic-form-6" name="interested" class="form-control">
+                                                    <option value="none" selected disabled>Interested in</option>
+                                                    <option value="design">Design</option>
+                                                    <option value="development">Development</option>
+                                                    <option value="illustration">Illustration</option>
+                                                    <option value="branding">Branding</option>
+                                                    <option value="video">Video</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6 col-12">
+                                            <div class="form-group mb-2">
+                                                <label for="basic-form-7">Budget</label>
+                                                <select id="basic-form-7" name="budget" class="form-control">
+                                                    <option value="0" selected disabled>Budget</option>
+                                                    <option value="less than $5000">less than $5000</option>
+                                                    <option value="$5000 - $10000">$5000 - $10000</option>
+                                                    <option value="$10000 - $20000">$10000 - $20000</option>
+                                                    <option value="more than $20000">more than $20000</option>
+                                                </select>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-4">Contact Number</label>
-                                            <input type="number" id="basic-form-4" class="form-control" name="phone">
-                                        </div>
+                                    <div class="form-group mb-2">
+                                        <label for="basic-form-8">Select File</label>
+                                        <input type="file" class="form-control-file" id="basic-form-8">
                                     </div>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="basic-form-5">Company</label>
-                                    <input type="text" id="basic-form-5" class="form-control" name="company">
-                                </div>
-                                <div class="form-row">
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-6">Interested in</label>
-                                            <select id="basic-form-6" name="interested" class="form-control">
-                                                <option value="none" selected disabled>Interested in</option>
-                                                <option value="design">Design</option>
-                                                <option value="development">Development</option>
-                                                <option value="illustration">Illustration</option>
-                                                <option value="branding">Branding</option>
-                                                <option value="video">Video</option>
-                                            </select>
-                                        </div>
+                                    <div class="form-group mb-2">
+                                        <label for="basic-form-9">About Project</label>
+                                        <textarea id="basic-form-9" rows="4" class="form-control" name="comment"></textarea>
                                     </div>
-                                    <div class="col-md-6 col-12">
-                                        <div class="form-group mb-2">
-                                            <label for="basic-form-7">Budget</label>
-                                            <select id="basic-form-7" name="budget" class="form-control">
-                                                <option value="0" selected disabled>Budget</option>
-                                                <option value="less than $5000">less than $5000</option>
-                                                <option value="$5000 - $10000">$5000 - $10000</option>
-                                                <option value="$10000 - $20000">$10000 - $20000</option>
-                                                <option value="more than $20000">more than $20000</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="basic-form-8">Select File</label>
-                                    <input type="file" class="form-control-file" id="basic-form-8">
-                                </div>
-                                <div class="form-group mb-2">
-                                    <label for="basic-form-9">About Project</label>
-                                    <textarea id="basic-form-9" rows="4" class="form-control" name="comment"></textarea>
-                                </div>
-                                <button type="button" class="btn btn-primary mr-2"><i class="ft-check-square mr-1"></i>Save</button>
-                                <button type="button" class="btn btn-secondary"><i class="ft-x mr-1"></i>Cancel</button>
-                            </form>
+                                    <button type="button" class="btn btn-primary mr-2"><i class="ft-check-square mr-1"></i>Save</button>
+                                    <button type="button" class="btn btn-secondary"><i class="ft-x mr-1"></i>Cancel</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn bg-light-secondary" data-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
