57081b86087119d614e4b4928f733d1da9bea840
KW1C-MonsantoJoshua <joshua.monsanto@edu-kw1c.nl>
2022-02-03 15:08:23 +0100

    link plaatjes

--- backend/wachtwoord_new.php
+++ backend/wachtwoord_new.php
@@ -52,7 +52,7 @@ if (isset($_POST['subforgot'])) {
             <div class="card overflow-hidden">
                 <div class="row m-0">
                     <div class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center text-center auth-img-bg py-2">
-                        <img src="../assets/img/gallery/forgot.png" alt="" class="img-fluid" width="260" height="230">
+                        <img src="../assets/img/logo2.png" alt="" class="img-fluid" width="260" height="230">
                     </div>
                     <div class="col-lg-6 col-md-12 px-4 py-3">
 
@@ -62,7 +62,7 @@ if (isset($_POST['subforgot'])) {
                         <form method="post">
                             <input name="email" type="email" class="form-control mb-3" placeholder="Email">
                             <div class="d-flex flex-sm-row flex-column justify-content-between">
-                                <a href="auth-login.html" class="btn bg-light-primary mb-2 mb-sm-0">Back To Login</a>
+                                <a href="index.php" class="btn bg-light-primary mb-2 mb-sm-0">Back To Login</a>
                                 <button class="btn btn-primary ml-sm-1" type="submit" name="subforgot">Versturen
                                 </button>
                             </div>
