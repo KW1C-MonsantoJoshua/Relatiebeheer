f47158fe9d8b9371140d11f3dd76065157b251fa
TeusROC <40191531@roctilburg.nl>
2022-02-08 10:58:06 +0100

    Notities laten zien

--- bedrijfprofieltest.php
+++ bedrijfprofieltest.php
@@ -1408,7 +1408,25 @@ include "partials/navbar.php";
                                                             Meest recente notitiess</h3>
                                                         <span class="label label-info">2</span>
                                                     </div>
-                                                    <?php ViewNote(); ?>
+                                                    <div class="col-sm-5 col-md-6 col-12 pb-4">
+                                                        <h1>Comments</h1>
+                                                        <div class="comment mt-4 text-justify float-left"> <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40">
+                                                            <h4>Jhon Doe</h4> <span>- 20 October, 2018</span> <br>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                                                        </div>
+                                                        <div class="text-justify darker mt-4 float-right"> <img src="https://i.imgur.com/CFpa3nK.jpg" alt="" class="rounded-circle" width="40" height="40">
+                                                            <h4>Rob Simpson</h4> <span>- 20 October, 2018</span> <br>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                                                        </div>
+                                                        <div class="comment mt-4 text-justify"> <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40">
+                                                            <h4>Jhon Doe</h4> <span>- 20 October, 2018</span> <br>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                                                        </div>
+                                                        <div class="darker mt-4 text-justify"> <img src="https://i.imgur.com/CFpa3nK.jpg" alt="" class="rounded-circle" width="40" height="40">
+                                                            <h4>Rob Simpson</h4> <span>- 20 October, 2018</span> <br>
+                                                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
