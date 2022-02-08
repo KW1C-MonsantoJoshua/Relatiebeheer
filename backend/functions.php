d223b3df973efa17ecd1ff2ec7ca3328f0e01971
TeusROC <40191531@roctilburg.nl>
2022-02-08 10:45:17 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2341,7 +2341,7 @@ while ($RowNote = $resultData->fetch_array()) {
                                                                 </br>
                                                                     <div class="col-xs-2 col-md-1">
                                                                         <img src="uploads/<?= $RowNoteGebruiker['image_url'] ?>"
-                                                                             class=" img-responsive avatar avatar-xl"
+                                                                             class="avatar avatar-xl"
                                                                              alt=""/></div>
                                                                     <div class="col-xs-10 col-md-11">
                                                                         <div>
