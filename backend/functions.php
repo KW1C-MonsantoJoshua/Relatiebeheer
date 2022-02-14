d1cac09dec39c4e476557671d3e434f312b4fc3d
TeusROC <40191531@roctilburg.nl>
2022-02-14 10:36:02 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2486,7 +2486,7 @@ while ($RowNote = $resultData->fetch_array()) {
                 <div class="row">
                     <div class="comment mt-4 text-justify float-left"> <img src="uploads/<?= $RowNoteGebruiker['image_url'] ?>" alt="" class="rounded-circle" width="40" height="40">
                         <h4><?php echo $RowNoteGebruiker["username"]; ?></h4> <span><?php echo $RowNote["created_at"]; ?></span> <br>
-                        <h5><?php echo $RowNoteGebruiker["subject"]; ?></h5><br>
+                        <h5><?php echo $RowNote["subject"]; ?></h5><br>
                         <p><?php echo $RowNote["text"]; ?></p>
                         <div class="action">
                             <button type="button"
