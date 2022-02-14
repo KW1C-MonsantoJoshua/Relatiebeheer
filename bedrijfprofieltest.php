a36512e5da7d4789f18b9d81b38a6a6726baaca0
TeusROC <40191531@roctilburg.nl>
2022-02-14 13:03:12 +0100

    Notities laten zien

--- bedrijfprofieltest.php
+++ bedrijfprofieltest.php
@@ -28,6 +28,8 @@ EditNoteExtra();
 <html class="loading" lang="en">
 <head>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
     <style>
     .navbar-nav {
         width: 100%
