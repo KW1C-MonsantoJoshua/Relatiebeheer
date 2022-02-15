c7529f59d41ca071366ccedaf4b1dc1f3ab1f70c
TeusROC <40191531@roctilburg.nl>
2022-02-15 15:34:06 +0100

    Notities laten zien

--- bedrijf_profiel.php
+++ bedrijf_profiel.php
@@ -41,7 +41,7 @@ EditNoteExtra();
         padding-left: 40px;
         padding-right: 30px;
         padding-top: 10px;
-        width: fit-content;
+        width: 850px;
     }
     .comment5 h4,
     .comment5 span,
