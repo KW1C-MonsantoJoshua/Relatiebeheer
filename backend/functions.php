5ba4aed7baadc3cab5fd8623d699c3c1b9071575
TeusROC <40191531@roctilburg.nl>
2022-02-15 14:41:18 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2405,9 +2405,10 @@ function ViewNote2()
                     <div class="row">
                         <div class="comment mt-4 text-justify float-left"> <img src="uploads/<?= $RowNoteGebruiker['image_url'] ?>" alt="" class="rounded-circle" width="40" height="40">
 
-
                             <h4><?php echo $RowNoteGebruiker["username"]; ?></h4>
+                            <div style="float:right">
                             <span><?php echo $RowNote["created_at"]; ?></span>
+                            </div>
 
                             <br>
                             <div class="BRC" style="line-height:15%;"> </br> </div>
