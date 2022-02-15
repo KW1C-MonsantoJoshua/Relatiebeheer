fbda4bdda1bab9d68a5b089a23a5fe454b2809d2
TeusROC <40191531@roctilburg.nl>
2022-02-15 14:26:03 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2405,14 +2405,14 @@ function ViewNote2()
                     <div class="row">
                         <div class="comment mt-4 text-justify float-left"> <img src="uploads/<?= $RowNoteGebruiker['image_url'] ?>" alt="" class="rounded-circle" width="40" height="40">
 
-                            <div class="float-parent-element">
-                                <div class="float-child-element">
-                                    <div class="red">Float Column 1</div>
-                                </div>
-                                <div class="float-child-element">
-                                    <div class="yellow">Float Column 2</div>
-                                </div>
-                            </div>
+<!--                            <div class="float-parent-element">-->
+<!--                                <div class="float-child-element">-->
+<!--                                    <div class="red">Float Column 1</div>-->
+<!--                                </div>-->
+<!--                                <div class="float-child-element">-->
+<!--                                    <div class="yellow">Float Column 2</div>-->
+<!--                                </div>-->
+<!--                            </div>-->
 
                             <h4><?php echo $RowNoteGebruiker["username"]; ?></h4>
                             <span><?php echo $RowNote["created_at"]; ?></span>
