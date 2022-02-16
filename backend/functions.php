48298ce3a68863ddd2ef8cdf06841e4a020dddaf
TeusROC <40191531@roctilburg.nl>
2022-02-16 14:10:58 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2424,7 +2424,7 @@ function ViewNote2()
 
                             <h4><?php echo $RowNoteGebruiker["username"]; ?></h4>
                             <div class="kip1">
-                                <span><?php echo $RowNote["keuze"]; ?></span>
+                                <span style="margin-right: 10px;"><b><?php echo $RowNote["keuze"]; ?></b></span>
                             <span><?php echo $creation_time = date('d-m-Y h:i', strtotime($RowNote['created_at'])); ?></span>
                             </div>
 
