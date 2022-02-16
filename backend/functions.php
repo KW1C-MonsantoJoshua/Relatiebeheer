f17b49234d384b16c205547da59c6e86fafe14b4
TeusROC <40191531@roctilburg.nl>
2022-02-16 11:27:55 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2391,7 +2391,7 @@ function ViewNote2()
     $stmt = $mysqli->prepare($tableData);
     $stmt->execute();
     $resultData = $stmt->get_result();
-    while ($RowNote = array_reverse($resultData,true)->fetch_array())
+    while ($RowNote = array_reverse($resultData->fetch_array()))
     {
         $idgebuiker = $RowNote["created_by"];
         $tableData1 = "SELECT * FROM `users` where `id` = '$idgebuiker'";
