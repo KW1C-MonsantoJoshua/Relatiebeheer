cb6326b9531ed0ced2ce3c60a23a370d543f285f
TeusROC <40191531@roctilburg.nl>
2022-02-16 13:05:01 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2392,9 +2392,8 @@ function ViewNote2()
     $stmt->execute();
     $resultData = $stmt->get_result();
     $kip3 = $resultData->fetch_array();
-    $kip4 = array_reverse($kip3, true);
     var_dump($kip3);
-    while ($RowNote = $resultData->fetch_array())
+    while ($RowNote = $kip3->fetch_array())
     {
         $idgebuiker = $RowNote["created_by"];
         $tableData1 = "SELECT * FROM `users` where `id` = '$idgebuiker'";
