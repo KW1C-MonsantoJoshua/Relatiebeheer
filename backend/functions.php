ae17efaaaf2964dcf518399d099820d93a9b0efa
TeusROC <40191531@roctilburg.nl>
2022-02-16 13:08:26 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -2395,7 +2395,7 @@ function ViewNote2()
     $kip4 = array_reverse($kip3, true);
     var_dump($kip3);
     var_dump($kip4);
-
+    echo count($kip4);
     while ($RowNote = $resultData->fetch_array())
     {
         $idgebuiker = $RowNote["created_by"];
