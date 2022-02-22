4b1f5e4c1661f632e08b22c374903a66df2071c7
TeusROC <40191531@roctilburg.nl>
2022-02-22 12:53:16 +0100

    Notities laten zien

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -5,13 +5,13 @@ if (!isset($_SESSION["loggedin"])) {
     header("Location: index.php");
 }
 $row2 = Getuser();
-if (!($row2['member_of'] === $_GET['membof'])) {
-    $memb_of = $row2['member_of'];
-    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-} else if (!($row2['member_of'] === $_GET['custof'])) {
-    $memb_of = $row2['member_of'];
-    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-}
+//if (!($row2['member_of'] === $_GET['membof'])) {
+//    $memb_of = $row2['member_of'];
+//    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+//} else if (!($row2['member_of'] === $_GET['custof'])) {
+//    $memb_of = $row2['member_of'];
+//    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+//}
 UpdateCompanyInfo();
 $rowC = GetCompanyInfo();
 editPersonnel();
