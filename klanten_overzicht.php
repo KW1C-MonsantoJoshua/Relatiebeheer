8be618b3ab6d93735974aef7bc5a6c685e9acead
TeusROC <40191531@roctilburg.nl>
2022-02-23 16:25:08 +0100

    Notities laten zien

--- klanten_overzicht.php
+++ klanten_overzicht.php
@@ -9,14 +9,14 @@ $row2 = Getuser();
 //        $memb_of = $row2['member_of'];
 //        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
 //    }
-if (!($row2['authentication_level'] === "Admin")) {
-    if (!($row2['member_of'] === $_GET['membof'])) {
-        $memb_of = $row2['member_of'];
-        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-    } else if (!($row2['member_of'] === $_GET['custof'])) {
-        $memb_of = $row2['member_of'];
-        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-    }
+if (!($row2['authentication_level'] = "Admin")) {
+if (!($row2['member_of'] == $_GET['membof'])) {
+    $memb_of = $row2['member_of'];
+    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+} else if (!($row2['member_of'] == $_GET['custof'])) {
+    $memb_of = $row2['member_of'];
+    header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+}
 }
 UpdateCompanyInfo();
 $rowC = GetCompanyInfo();
