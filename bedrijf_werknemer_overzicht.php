e09360d3605683dcf1d873568cad57310a2d45b4
TeusROC <40191531@roctilburg.nl>
2022-02-23 15:07:05 +0100

    Notities laten zien

--- bedrijf_werknemer_overzicht.php
+++ bedrijf_werknemer_overzicht.php
@@ -420,7 +420,7 @@ include "partials/footer.php";
             // de checker niffo
 
             postcode = num_deel + alpha_deel;
-            document.getElementById("postcode_w").value = postcode;
+            document.getElementById("postcode").value = postcode;
         }
 
         if (wat === "huisnr") {
@@ -429,12 +429,11 @@ include "partials/footer.php";
                 maak_leeg();
                 return;
             }
-            document.getElementById("huisnr_w").value = huisnr;
+            document.getElementById("huisnr").value = huisnr;
         }
 
         if (wat === "toevoeging") {
             toevoeging = waarde.trim();
-            document.getElementById("toevoeging_w").value = toevoeging;
         }
 
         if (huisnr === 0) {
@@ -471,8 +470,8 @@ include "partials/footer.php";
                 }
                 // 0 = straat - 1 = plaats
                 aResponse = rString.split(";");
-                document.getElementById("straat_w").value = aResponse[0];
-                document.getElementById("plaats_w").value = aResponse[1];
+                document.getElementById("straat").value = aResponse[0];
+                document.getElementById("plaats").value = aResponse[1];
             }
         };
 
@@ -481,8 +480,8 @@ include "partials/footer.php";
     }
 
     function maak_leeg() {
-        document.getElementById("straat_w").value = "";
-        document.getElementById("plaats_w").value = "";
+        document.getElementById("").value = "";
+        document.getElementById("plaats").value = "";
     }
 </script>
 
