79ab3b5b3ab6b3b7cbc6ae63f0dd9543690035d5
TeusROC <40191531@roctilburg.nl>
2022-02-23 11:28:08 +0100

    Notities laten zien

--- partials/navbar.php
+++ partials/navbar.php
@@ -158,7 +158,7 @@ $row = Getuser();
                 </li>
                 <li class="nav-item">
                     <?php
-                    if(isset($_GET['member_of'])) {
+                    if(isset($_GET['membof'])) {
                     if ($row["authentication_level"] === 'Admin' || 'Bedrijfsleider') {
                         ?>
                         <a class="" href="bedrijfs_overzicht.php">
