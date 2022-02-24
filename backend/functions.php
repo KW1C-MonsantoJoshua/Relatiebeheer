c5f3f45d2f0a5e954edd770a8671688ae59672a8
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-02-24 13:17:44 +0100

    facturen

--- backend/functions.php
+++ backend/functions.php
@@ -2612,6 +2612,6 @@ function Createinvoice()
         $pdf->writeHTML("Katie A Falk, (07) 4050 2235, katie@sks.com", true, false, false, false, 'C');
 
 // save pdf file
-        $pdf->Output(__DIR__ . '/invoice#12.pdf', 'F');
+        $pdf->Output(__DIR__ . '../facturen/invoice#12.pdf', 'F');
     }
 }
