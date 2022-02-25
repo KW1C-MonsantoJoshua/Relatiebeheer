b0b0c0a772bf2b0fc06f9a3850aed20627270e01
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-02-25 10:23:13 +0100

    facturen

--- backend/functions.php
+++ backend/functions.php
@@ -2613,7 +2613,7 @@ function Createinvoice()
 
 // save pdf file
 
-        $pdf->Output('/facturen/filename.pdf', 'F');
+        $pdf->Output('/var/www/vhosts/relatiebeheer.qccstest.nl/httpdocs/facturen/filename.pdf', 'F');
 //        $pdf->Output($_SERVER['DOCUMENT_ROOT'] . '/facturen/' . 'output.pdf', 'F');
         echo "succes";
     }else echo "fout1";
