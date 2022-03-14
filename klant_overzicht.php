65db0f87f3c88fb0b90a0c92f9d351a7cc9e2e55
TeusROC <40191531@roctilburg.nl>
2022-03-14 10:36:36 +0100

    Notities laten zien

--- klant_overzicht.php
+++ klant_overzicht.php
@@ -167,11 +167,6 @@ include "partials/navbar.php";
             </div>
         </div>
         <!-- END : End Main Content-->
-
-        <!-- BEGIN : Footer-->
-        <footer class="footer undefined undefined">
-            <p class="clearfix text-muted m-0"><span>Copyright &copy; 2020 &nbsp;</span><a href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" id="pixinventLink" target="_blank">PIXINVENT</a><span class="d-none d-sm-inline-block">, All rights reserved.</span></p>
-        </footer>
         <!-- End : Footer-->
         <!-- Scroll to top button -->
         <button class="btn btn-primary scroll-top" type="button"><i class="ft-arrow-up"></i></button>
