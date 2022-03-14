9b5d5e208d367eac4dea52f9518184dadceabdf2
TeusROC <40191531@roctilburg.nl>
2022-03-14 10:56:11 +0100

    Notities laten zien

--- klant_overzicht.php
+++ klant_overzicht.php
@@ -74,8 +74,8 @@ include "partials/navbar.php";
                                         <div class="card-body text-center p-4">
                                             <a href="page-knowledge-categories.html">
                                                 <i class="ft-user"></i>
-                                                <h5 class="mt-2">My Account</h5>
-                                                <p class="m-0 text-muted">But students more often neglect fact it is much more</p>
+                                                <h5 class="mt-2">Mijn Acccount</h5>
+                                                <p class="m-0 text-muted">Account gegevens wijzigen of bekijken</p>
                                             </a>
                                         </div>
                                     </div>
@@ -88,7 +88,7 @@ include "partials/navbar.php";
                                             <a href="page-knowledge-categories.html">
                                                 <i class="ft-link"></i>
                                                 <h5 class="mt-2">Connect</h5>
-                                                <p class="m-0 text-muted">But students more often neglect fact it is much more</p>
+                                                <p class="m-0 text-muted">Voer hier je connecties zoals google en linked in</p>
                                             </a>
                                         </div>
                                     </div>
@@ -100,8 +100,8 @@ include "partials/navbar.php";
                                         <div class="card-body text-center p-4">
                                             <a href="page-knowledge-categories.html">
                                                 <i class="ft-dollar-sign"></i>
-                                                <h5 class="mt-2">Charges & Refunds</h5>
-                                                <p class="m-0 text-muted">But students more often neglect fact it is much more</p>
+                                                <h5 class="mt-2">Facturen en betalingen</h5>
+                                                <p class="m-0 text-muted">Factuur en betaalgegevens bekijken</p>
                                             </a>
                                         </div>
                                     </div>
@@ -113,8 +113,8 @@ include "partials/navbar.php";
                                         <div class="card-body text-center p-4">
                                             <a href="page-knowledge-categories.html">
                                                 <i class="ft-globe"></i>
-                                                <h5 class="mt-2">International</h5>
-                                                <p class="m-0 text-muted">But students more often neglect fact it is much more</p>
+                                                <h5 class="mt-2">Hulp vragen</h5>
+                                                <p class="m-0 text-muted">Vraag om hulp of bekijk de videos</p>
                                             </a>
                                         </div>
                                     </div>
@@ -139,8 +139,8 @@ include "partials/navbar.php";
                                         <div class="card-body text-center p-4">
                                             <a href="page-knowledge-categories.html">
                                                 <i class="ft-alert-circle"></i>
-                                                <h5 class="mt-2">Disputes & Fraud</h5>
-                                                <p class="m-0 text-muted">But students more often neglect fact it is much more</p>
+                                                <h5 class="mt-2">Opmerkingen</h5>
+                                                <p class="m-0 text-muted">Bekijk hier alle opmerkingen</p>
                                             </a>
                                         </div>
                                     </div>
