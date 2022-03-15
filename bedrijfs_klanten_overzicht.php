fa40d9f82561a322f94e2fa87d3611e24f4e5ef2
TeusROC <40191531@roctilburg.nl>
2022-03-15 11:45:56 +0100

    Notities laten zien

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -18,14 +18,14 @@ if (!($row2['authentication_level'] === 'Admin')) {
         header("Location:../klant_overzicht.php?custof=$memb_of&membof=$memb_of");
     }
     }
-//    if (!($row2['member_of'] === $_GET['membof'])) {
-//        $memb_of = $row2['member_of'];
-//        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-//    }
-//    if (!($row2['member_of'] === $_GET['custof'])) {
-//        $memb_of = $row2['member_of'];
-//        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-//    }
+    if (!($row2['member_of'] === $_GET['membof'])) {
+        $memb_of = $row2['member_of'];
+        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+    }
+    if (!($row2['member_of'] === $_GET['custof'])) {
+        $memb_of = $row2['member_of'];
+        header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+    }
 }
 
 InsertCustomerIndividual();
