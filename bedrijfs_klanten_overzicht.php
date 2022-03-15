a01aafaa2aa5fd37fe2f1d6cc678f93c58fe576a
TeusROC <40191531@roctilburg.nl>
2022-03-15 11:58:14 +0100

    Notities laten zien

--- bedrijfs_klanten_overzicht.php
+++ bedrijfs_klanten_overzicht.php
@@ -9,14 +9,18 @@ if (!isset($_SESSION["loggedin"])) {
 $row2 = Getuser();
 if (!($row2['authentication_level'] === 'Admin')) {
     if (!($row2['authentication_level'] === 'Bedrijfsleider')) {
-    if ($row2['authentication_level'] === 'Werknemer') {
-        $memb_of = $row2['member_of'];
-        header("Location:../klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+        if ($row2['authentication_level'] === 'Werknemer') {
+            $memb_of = $row2['member_of'];
+            header("Location:../klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
+        }
+        if ($row2['authentication_level'] === 'user') {
+            $memb_of = $row2['member_of'];
+            header("Location:../klant_overzicht.php?custof=$memb_of&membof=$memb_of");
+        }
     }
-    if ($row2['authentication_level'] === 'user') {
+    if ($row2['member_of'] !== $_GET['membof']) {
         $memb_of = $row2['member_of'];
         header("Location:../klant_overzicht.php?custof=$memb_of&membof=$memb_of");
-    }
     }
 }
     echo $_GET['membof'];
