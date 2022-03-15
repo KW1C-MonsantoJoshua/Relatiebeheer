ee3f16c45178137995bda0a3c8d0d1d2769bdb9b
TeusROC <40191531@roctilburg.nl>
2022-03-15 11:05:19 +0100

    Notities laten zien

--- bedrijf_werknemer_overzicht.php
+++ bedrijf_werknemer_overzicht.php
@@ -14,10 +14,10 @@ if (!($row2['authentication_level'] === 'Admin')) {
         $memb_of = $row2['member_of'];
         header("Location:../klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
     }
-    if (!($row2['member_of'] == $_GET['membof'])) {
+    if ($row2['member_of'] !== $_GET['membof']) {
         $memb_of = $row2['member_of'];
         header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
-    } else if (!($row2['member_of'] == $_GET['custof'])) {
+    } else if ($row2['member_of'] !== $_GET['custof']) {
         $memb_of = $row2['member_of'];
         header("Location:../bedrijfs_klanten_overzicht.php?custof=$memb_of&membof=$memb_of");
     }
