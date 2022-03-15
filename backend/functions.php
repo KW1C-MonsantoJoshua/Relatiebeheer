9facf405a3238c0b1cb2b126d1976c272d6b40f0
TeusROC <40191531@roctilburg.nl>
2022-03-15 10:27:59 +0100

    Notities laten zien

--- backend/functions.php
+++ backend/functions.php
@@ -513,7 +513,7 @@ function InsertCustomerIndividual()
                     $stmt->close();
 
 
-                    $sql = "INSERT INTO `users`(`id`, `email`, `username`, `authentication_level`, `name`, `member_of`, `reset_token`) VALUES (?,?,?,?,?,?)";
+                    $sql = "INSERT INTO `users`(`id`, `email`, `username`, `authentication_level`, `name`, `member_of`, `reset_token`) VALUES (?,?,?,?,?,?,?)";
                     $stmt = $mysqli->prepare($sql);
                     $token = bin2hex(random_bytes(50));
                     $authentication = 'user';
