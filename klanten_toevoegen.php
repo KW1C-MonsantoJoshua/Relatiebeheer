55707ac15442c7e9f01f44704b362cb36567d02f
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-05-12 13:25:38 +0200

    Formulier

--- klanten_toevoegen.php
+++ klanten_toevoegen.php
@@ -17,10 +17,10 @@ $users = new userActions();
 
 if (isset($_POST['registreerParticulier'])) {
     echo $users->registerUsersP($_POST['voornaam_p'],$_POST['tussenvoegsel_p'], $_POST['achternaam_p'], $_POST['straatnaam_p'], $_POST['huisnummer_p'], $_POST['postcode_p'], $_POST['telefoonnummer_p'], $_POST['email_p'], $_POST['bedrijf']);
-}
+}else echo "fout1";
 if (isset($_POST['registreerZakelijk'])) {
     echo $users->registerUsersZ($_POST['voornaam_z'],$_POST['tussenvoegsel_z'], $_POST['achternaam_z'], $_POST['straatnaam_z'], $_POST['huisnummer_z'], $_POST['postcode_z'], $_POST['telefoonnummer_z'], $_POST['email_z'], $_POST['bedrijfsnaam'], $_POST['bedrijf']);
-}
+}else echo "fout2";
 ?>
 <!-- END : Head-->
 
