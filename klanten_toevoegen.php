8344a97b9c428ffa1791772b3080ea116a6919ca
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-05-24 14:51:03 +0200

    token

--- klanten_toevoegen.php
+++ klanten_toevoegen.php
@@ -81,10 +81,10 @@ $users = new userActions();
                                                                 $stmt->execute();
                                                                 $token_check = $stmt->fetch();
                                                                 if ($token_check){
+                                                                    $stmt_d = $mysqli->prepare("DELETE FROM token WHERE token = ?");
                                                                     echo $token;
-                                                                    $stmt_delete = $mysqli->prepare("DELETE * FROM `token` WHERE token = ?");
-                                                                    $stmt_delete->bind_param("s", $token);
-                                                                    $stmt_delete->execute();
+                                                                    $stmt_d->bind_param("s", $token);
+                                                                    $stmt_d->execute();
                                                                     echo $users->registerUsersP($_POST['voornaam_p'],$_POST['tussenvoegsel_p'], $_POST['achternaam_p'], $_POST['straatnaam_p'], $_POST['huisnummer_p'], $_POST['postcode_p'], $_POST['telefoonnummer_p'], $_POST['email_p'], $_GET['membof']);
                                                                     echo "<p class='text-success'>Relatie succesvol toegevoegd !</p>";
                                                                 }else {
