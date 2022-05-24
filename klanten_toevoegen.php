0889e1bc0261dd6fba5df102dc9b0655e02fc7ad
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-05-24 13:29:58 +0200

    token

--- klanten_toevoegen.php
+++ klanten_toevoegen.php
@@ -89,7 +89,7 @@ $users = new userActions();
                                                                     if ($stmt_d->execute()){
                                                                         echo $users->registerUsersP($_POST['voornaam_p'],$_POST['tussenvoegsel_p'], $_POST['achternaam_p'], $_POST['straatnaam_p'], $_POST['huisnummer_p'], $_POST['postcode_p'], $_POST['telefoonnummer_p'], $_POST['email_p'], $_GET['membof']);
                                                                         echo "<p class='text-success'>Relatie succesvol toegevoegd !</p>";
-                                                                    }
+                                                                    }else echo "fout3";
                                                                 }else {
                                                                     echo "<p class='text-danger'>Token is niet meer geldig !</p>";
                                                                 }
