55f3e3e3a5a72c82f1979a4e006d31b81cef3c86
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-05-24 13:19:17 +0200

    token

--- klanten_toevoegen.php
+++ klanten_toevoegen.php
@@ -78,7 +78,10 @@ $users = new userActions();
                                                                 $token = $_GET['token'];
                                                                 $stmt = $mysqli->prepare("SELECT * FROM `token` WHERE token = ?");
                                                                 $stmt->bind_param("s", $token);
-                                                                if ($stmt->execute()){
+                                                                $stmt->execute();
+                                                                $stmt->fetch();
+                                                                $stmt->store_result();
+                                                                if ($stmt->num_rows > 0{
                                                                     $data = $stmt->get_result();
                                                                     $idtoken = $data->fetch_array();
                                                                     $stmt_d = $mysqli->prepare("DELETE FROM token WHERE idtoken = ?");
