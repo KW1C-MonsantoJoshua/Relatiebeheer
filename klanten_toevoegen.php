6ea93207c8e93988183714043264228c81669dad
DayalManbodh <dayal.manbodh@ictmbo.nl>
2022-05-24 14:41:37 +0200

    token

--- klanten_toevoegen.php
+++ klanten_toevoegen.php
@@ -80,12 +80,13 @@ $users = new userActions();
                                                                 $stmt->bind_param("s", $token);
                                                                 $stmt->execute();
                                                                 $token_check = $stmt->fetch();
+                                                                echo $token;
                                                                 if ($token_check){
                                                                     $stmt_delete = $mysqli->prepare("DELETE FROM `token` WHERE token = ?");
                                                                     $stmt_delete->bind_param("s", $token);
                                                                     $stmt_delete->execute();
-                                                                        echo $users->registerUsersP($_POST['voornaam_p'],$_POST['tussenvoegsel_p'], $_POST['achternaam_p'], $_POST['straatnaam_p'], $_POST['huisnummer_p'], $_POST['postcode_p'], $_POST['telefoonnummer_p'], $_POST['email_p'], $_GET['membof']);
-                                                                        echo "<p class='text-success'>Relatie succesvol toegevoegd !</p>";
+                                                                    echo $users->registerUsersP($_POST['voornaam_p'],$_POST['tussenvoegsel_p'], $_POST['achternaam_p'], $_POST['straatnaam_p'], $_POST['huisnummer_p'], $_POST['postcode_p'], $_POST['telefoonnummer_p'], $_POST['email_p'], $_GET['membof']);
+                                                                    echo "<p class='text-success'>Relatie succesvol toegevoegd !</p>";
                                                                 }else {
                                                                     echo "<p class='text-danger'>Token is niet meer geldig !</p>";
                                                                 }
